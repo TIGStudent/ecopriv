332dc66899d6d01d6105be2d5f2f6fb6552592a2
TIGStudent <131448346+TIGStudent@users.noreply.github.com>
2024-09-05 11:23:56 +0200

    Moved window resize and movement logic to a separate file

--- ecopriv/MainWindow.xaml.cs
+++ ecopriv/MainWindow.xaml.cs
@@ -2,29 +2,40 @@
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using ecopriv.UI;
 
 namespace ecopriv
 {
     public partial class MainWindow : Window
     {
-        private bool isResizing;
-        private ResizeDirection resizeDirection;
-        private Point initialMousePosition;
-        private Rect initialWindowBounds;
+
+        private WindowDragBehavior _windowDragBehavior;
 
         public MainWindow()
         {
             InitializeComponent();
+
+            this.DataContext = new ViewModel();
+            _windowDragBehavior = new WindowDragBehavior();
         }
 
         private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            if (e.ChangedButton == MouseButton.Left)
-            {
-                this.DragMove();
-            }
+            _windowDragBehavior.TitleBar_MouseLeftButtonDown(this, e);
+        }
+        private void ResizeHandle_MouseMove(object sender, MouseEventArgs e)
+        {
+            _windowDragBehavior.ResizeHandle_MouseMove(this, sender, e);
+        }
+        private void ResizeHandle_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _windowDragBehavior.ResizeHandle_MouseLeftButtonDown(this, sender, e);
         }
 
+        private void ResizeHandle_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            _windowDragBehavior.ResizeHandle_MouseLeftButtonUp(sender, e);
+        }
         private void MinimizeButton_Click(object sender, RoutedEventArgs e)
         {
             this.WindowState = WindowState.Minimized;
@@ -35,153 +46,6 @@ namespace ecopriv
             this.Close();
         }
 
-        private void ResizeHandle_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ChangedButton == MouseButton.Left)
-            {
-                isResizing = true;
-                resizeDirection = GetResizeDirection(sender as Border);
-                initialMousePosition = e.GetPosition(this);
-                initialWindowBounds = new Rect(this.Left, this.Top, this.Width, this.Height);
-                Mouse.Capture(sender as IInputElement);
-            }
-        }
-
-        private void ResizeHandle_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (isResizing)
-            {
-                var currentPosition = e.GetPosition(this);
-                double deltaX = currentPosition.X - initialMousePosition.X;
-                double deltaY = currentPosition.Y - initialMousePosition.Y;
-
-                switch (resizeDirection)
-                {
-                    case ResizeDirection.Bottom:
-                        Height = initialWindowBounds.Height + deltaY;
-                        break;
-
-                    case ResizeDirection.Top:
-                        // Decrease height and move the window up
-                        Height = initialWindowBounds.Height - deltaY;
-                        Top = initialWindowBounds.Top + deltaY;
-                        break;
-
-                    case ResizeDirection.Right:
-                        Width = initialWindowBounds.Width + deltaX;
-                        break;
-
-                    case ResizeDirection.Left:
-                        // Decrease width and move the window left
-                        Width = initialWindowBounds.Width - deltaX;
-                        Left = initialWindowBounds.Left + deltaX;
-                        break;
-
-                    case ResizeDirection.BottomRight:
-                        // Increase both height and width
-                        Width = initialWindowBounds.Width + deltaX;
-                        Height = initialWindowBounds.Height + deltaY;
-                        break;
-
-                    case ResizeDirection.BottomLeft:
-                        // Increase height, decrease width, and move the window left
-                        Width = initialWindowBounds.Width - deltaX;
-                        Height = initialWindowBounds.Height + deltaY;
-                        Left = initialWindowBounds.Left + deltaX;
-                        break;
-
-                    case ResizeDirection.TopRight:
-                        // Decrease height, increase width, and move the window up
-                        Width = initialWindowBounds.Width + deltaX;
-                        Height = initialWindowBounds.Height - deltaY;
-                        Top = initialWindowBounds.Top + deltaY;
-                        break;
-
-                    case ResizeDirection.TopLeft:
-                        // Decrease both height and width, and move the window up and left
-                        Width = initialWindowBounds.Width - deltaX;
-                        Height = initialWindowBounds.Height - deltaY;
-                        Top = initialWindowBounds.Top + deltaY;
-                        Left = initialWindowBounds.Left + deltaX;
-                        break;
-                }
-            }
-        }
-
-
-
-        private void ResizeHandle_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
-        {
-            if (isResizing)
-            {
-                isResizing = false;
-                Mouse.Capture(null);
-            }
-        }
-
-        private ResizeDirection GetResizeDirection(Border border)
-        {
-            if (border == null)
-                return ResizeDirection.None;
-
-            var cursor = border.Cursor;
-            var horizontalAlignment = border.HorizontalAlignment;
-            var verticalAlignment = border.VerticalAlignment;
-
-            if (cursor == Cursors.SizeNS)
-            {
-                if (verticalAlignment == VerticalAlignment.Bottom)
-                    return ResizeDirection.Bottom;
-                if (verticalAlignment == VerticalAlignment.Top)
-                    return ResizeDirection.Top;
-            }
-            else if (cursor == Cursors.SizeWE)
-            {
-                if (horizontalAlignment == HorizontalAlignment.Right)
-                    return ResizeDirection.Right;
-                if (horizontalAlignment == HorizontalAlignment.Left)
-                    return ResizeDirection.Left;
-            }
-            else if (cursor == Cursors.SizeNWSE)
-            {
-                if (horizontalAlignment == HorizontalAlignment.Right && verticalAlignment == VerticalAlignment.Bottom)
-                    return ResizeDirection.BottomRight;
-                if (horizontalAlignment == HorizontalAlignment.Left && verticalAlignment == VerticalAlignment.Top)
-                    return ResizeDirection.TopLeft;
-                if (horizontalAlignment == HorizontalAlignment.Right && verticalAlignment == VerticalAlignment.Top)
-                    return ResizeDirection.TopRight;
-                if (horizontalAlignment == HorizontalAlignment.Left && verticalAlignment == VerticalAlignment.Bottom)
-                    return ResizeDirection.BottomLeft;
-            }
-            else if (cursor == Cursors.SizeNESW)
-            {
-                if (horizontalAlignment == HorizontalAlignment.Left && verticalAlignment == VerticalAlignment.Bottom)
-                    return ResizeDirection.BottomLeft;
-                if (horizontalAlignment == HorizontalAlignment.Right && verticalAlignment == VerticalAlignment.Top)
-                    return ResizeDirection.TopRight;
-                if (horizontalAlignment == HorizontalAlignment.Left && verticalAlignment == VerticalAlignment.Top)
-                    return ResizeDirection.TopLeft;
-                if (horizontalAlignment == HorizontalAlignment.Right && verticalAlignment == VerticalAlignment.Bottom)
-                    return ResizeDirection.BottomRight;
-            }
-
-            return ResizeDirection.None;
-        }
-
-
-
-        private enum ResizeDirection
-        {
-            None,
-            Top,
-            Bottom,
-            Left,
-            Right,
-            TopLeft,
-            TopRight,
-            BottomLeft,
-            BottomRight
-        }
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
             // Toggle visibility of AddBox
@@ -196,7 +60,6 @@ namespace ecopriv
                 AddButton.Content = "Open Add";
             }
         }
-
         private void AddSubmit_Click(object sender, RoutedEventArgs e)
         {
             // Retrieve values from the TextBoxes
---
+++ ecopriv/UI/WindowDragBehavior.cs
@@ -0,0 +1,168 @@
+﻿using ecopriv.Models;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ecopriv.UI
+{
+    public class WindowDragBehavior
+    {
+
+        private bool isResizing;
+        private ResizeDirection resizeDirection;
+        private Point initialMousePosition;
+        private Rect initialWindowBounds;
+        public void TitleBar_MouseLeftButtonDown(Window window, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                window.DragMove();
+            }
+        }
+        public void ResizeHandle_MouseLeftButtonDown(Window window, object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                isResizing = true;
+                resizeDirection = GetResizeDirection(sender as Border);
+                initialMousePosition = e.GetPosition(window);
+                initialWindowBounds = new Rect(window.Left, window.Top, window.Width, window.Height);
+                Mouse.Capture(sender as IInputElement);
+            }
+        }
+
+        public void ResizeHandle_MouseMove(Window window, object sender, MouseEventArgs e)
+        {
+            if (isResizing)
+            {
+                var currentPosition = e.GetPosition(window);
+                double deltaX = currentPosition.X - initialMousePosition.X;
+                double deltaY = currentPosition.Y - initialMousePosition.Y;
+
+                switch (resizeDirection)
+                {
+                    case ResizeDirection.Bottom:
+                        window.Height = initialWindowBounds.Height + deltaY;
+                        break;
+
+                    case ResizeDirection.Top:
+                        // Decrease height and move the window up
+                        window.Height = initialWindowBounds.Height - deltaY;
+                        window.Top = initialWindowBounds.Top + deltaY;
+                        break;
+
+                    case ResizeDirection.Right:
+                        window.Width = initialWindowBounds.Width + deltaX;
+                        break;
+
+                    case ResizeDirection.Left:
+                        // Decrease width and move the window left
+                        window.Width = initialWindowBounds.Width - deltaX;
+                        window.Left = initialWindowBounds.Left + deltaX;
+                        break;
+
+                    case ResizeDirection.BottomRight:
+                        // Increase both height and width
+                        window.Width = initialWindowBounds.Width + deltaX;
+                        window.Height = initialWindowBounds.Height + deltaY;
+                        break;
+
+                    case ResizeDirection.BottomLeft:
+                        // Increase height, decrease width, and move the window left
+                        window.Width = initialWindowBounds.Width - deltaX;
+                        window.Height = initialWindowBounds.Height + deltaY;
+                        window.Left = initialWindowBounds.Left + deltaX;
+                        break;
+
+                    case ResizeDirection.TopRight:
+                        // Decrease height, increase width, and move the window up
+                        window.Width = initialWindowBounds.Width + deltaX;
+                        window.Height = initialWindowBounds.Height - deltaY;
+                        window.Top = initialWindowBounds.Top + deltaY;
+                        break;
+
+                    case ResizeDirection.TopLeft:
+                        // Decrease both height and width, and move the window up and left
+                        window.Width = initialWindowBounds.Width - deltaX;
+                        window.Height = initialWindowBounds.Height - deltaY;
+                        window.Top = initialWindowBounds.Top + deltaY;
+                        window.Left = initialWindowBounds.Left + deltaX;
+                        break;
+                }
+            }
+        }
+
+        public void ResizeHandle_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (isResizing)
+            {
+                isResizing = false;
+                Mouse.Capture(null);
+            }
+        }
+
+        private ResizeDirection GetResizeDirection(Border border)
+        {
+            if (border == null)
+                return ResizeDirection.None;
+
+            var cursor = border.Cursor;
+            var horizontalAlignment = border.HorizontalAlignment;
+            var verticalAlignment = border.VerticalAlignment;
+
+            if (cursor == Cursors.SizeNS)
+            {
+                if (verticalAlignment == VerticalAlignment.Bottom)
+                    return ResizeDirection.Bottom;
+                if (verticalAlignment == VerticalAlignment.Top)
+                    return ResizeDirection.Top;
+            }
+            else if (cursor == Cursors.SizeWE)
+            {
+                if (horizontalAlignment == HorizontalAlignment.Right)
+                    return ResizeDirection.Right;
+                if (horizontalAlignment == HorizontalAlignment.Left)
+                    return ResizeDirection.Left;
+            }
+            else if (cursor == Cursors.SizeNWSE)
+            {
+                if (horizontalAlignment == HorizontalAlignment.Right && verticalAlignment == VerticalAlignment.Bottom)
+                    return ResizeDirection.BottomRight;
+                if (horizontalAlignment == HorizontalAlignment.Left && verticalAlignment == VerticalAlignment.Top)
+                    return ResizeDirection.TopLeft;
+                if (horizontalAlignment == HorizontalAlignment.Right && verticalAlignment == VerticalAlignment.Top)
+                    return ResizeDirection.TopRight;
+                if (horizontalAlignment == HorizontalAlignment.Left && verticalAlignment == VerticalAlignment.Bottom)
+                    return ResizeDirection.BottomLeft;
+            }
+            else if (cursor == Cursors.SizeNESW)
+            {
+                if (horizontalAlignment == HorizontalAlignment.Left && verticalAlignment == VerticalAlignment.Bottom)
+                    return ResizeDirection.BottomLeft;
+                if (horizontalAlignment == HorizontalAlignment.Right && verticalAlignment == VerticalAlignment.Top)
+                    return ResizeDirection.TopRight;
+                if (horizontalAlignment == HorizontalAlignment.Left && verticalAlignment == VerticalAlignment.Top)
+                    return ResizeDirection.TopLeft;
+                if (horizontalAlignment == HorizontalAlignment.Right && verticalAlignment == VerticalAlignment.Bottom)
+                    return ResizeDirection.BottomRight;
+            }
+
+            return ResizeDirection.None;
+        }
+
+        private enum ResizeDirection
+        {
+            None,
+            Top,
+            Bottom,
+            Left,
+            Right,
+            TopLeft,
+            TopRight,
+            BottomLeft,
+            BottomRight
+        }
+
+
+    }
+}
